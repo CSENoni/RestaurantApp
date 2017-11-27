e1de32c8a3b3b07cc8d1bef87b472987d4b1509c
chals004 <chals004@umn.edu>
2017-11-27 12:21:36 -0600

    Matt's commit

--- app/src/main/java/com/hoangvo/restaurantapp/MainActivity.java
+++ app/src/main/java/com/hoangvo/restaurantapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.hoangvo.restaurantapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
