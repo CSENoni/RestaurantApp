f7e1d490044160c1f2ebdece4f4930abaa9cd731
bmarchiniak <bmarchiniak@gmail.com>
2017-12-11 12:20:41 -0600

    Various bug fixes and small changes.

--- app/src/main/java/com/hoangvo/restaurantapp/HelpActivity.java
+++ app/src/main/java/com/hoangvo/restaurantapp/HelpActivity.java
@@ -1,7 +1,54 @@
 package com.hoangvo.restaurantapp;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HelpActivity extends AppCompatActivity{
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.titlemenu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                Intent logoutIntent = new Intent(HelpActivity.this, MainActivity.class);
+                HelpActivity.this.startActivity(logoutIntent);
+                return true;
+            case R.id.home:
+                Intent optionIntent = new Intent(HelpActivity.this, OptionActivity.class);
+                HelpActivity.this.startActivity(optionIntent);
+                return true;
+            case R.id.mylist:
+                Intent listIntent = new Intent(HelpActivity.this, ListActivity.class);
+                HelpActivity.this.startActivity(listIntent);
+                return true;
+            case R.id.nearby:
+                Intent nearbyIntent = new Intent(HelpActivity.this, NearbyActivity.class);
+                HelpActivity.this.startActivity(nearbyIntent);
+                return true;
+            case R.id.groups:
+                Intent groupsIntent = new Intent(HelpActivity.this, GroupActivity.class);
+                HelpActivity.this.startActivity(groupsIntent);
+                return true;
+            case R.id.help:
+                Intent helpIntent = new Intent(HelpActivity.this, HelpActivity.class);
+                HelpActivity.this.startActivity(helpIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
 }
